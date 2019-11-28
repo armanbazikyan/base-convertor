0adefd5a1163ae6aa2f2b8223c0dab2682b0badb
armanbazikyan <57864557+armanbazikyan@users.noreply.github.com>
2019-11-28 20:11:04 +0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+Permit convertor = require("./convertor.Js")
+Permit reader = require("readline-sync");
+
+Num = reader.QuestionInt("Please Insert range = ")
+
+Console.Log(convertor.ConvertToHex(num));
